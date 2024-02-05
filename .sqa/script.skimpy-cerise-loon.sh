bb322cd9b494092b26f44dfd04ae792946acbd5a
orviz <orviz@ifca.unican.es>
2024-02-05 11:01:27 +0100

    Add JePL file structure

---
+++ .sqa/script.skimpy-cerise-loon.sh
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd github.com/MicroStep-MIS/UC-MicroStep-MIS-ai4eosc_thunder_nowcast_ml &&
+    find_doc_files.py --file_type all
+)
---
+++ .sqa/script.woozy-ecru-bandicoot.sh
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd github.com/MicroStep-MIS/UC-MicroStep-MIS-ai4eosc_thunder_nowcast_ml &&
+    get_git_tags.py
+)
